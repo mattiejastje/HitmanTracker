49d75b797ee6eb2058580873ac0c4296f23acd2d
mattiejas <mattiejastje@gmail.com>
2025-05-04 11:16:15 +0100

    Clean up ImGui example.

--- gui.cpp
+++ gui.cpp
@@ -10,11 +10,12 @@
 #pragma comment(lib, "d3d9.lib")
 
 #include <d3d9.h>
+#include <imgui.h>
+#include <imgui_impl_dx9.h>
+#include <imgui_impl_win32.h>
 #include <tchar.h>
 
-#include "imgui.h"
-#include "imgui_impl_dx9.h"
-#include "imgui_impl_win32.h"
+#include "logger.hpp"
 
 // Data
 static LPDIRECT3D9 g_pD3D = nullptr;
@@ -32,7 +33,7 @@ LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 // Main code
 int gui_run() {
     // Create application window
-    // ImGui_ImplWin32_EnableDpiAwareness();
+    spdlog::trace("Creating application window...");
     WNDCLASSEXW wc
         = {sizeof(wc),
            CS_CLASSDC,
@@ -53,8 +54,8 @@ int gui_run() {
         WS_OVERLAPPEDWINDOW,
         100,
         100,
-        1280,
-        800,
+        260,
+        520,
         nullptr,
         nullptr,
         wc.hInstance,
@@ -62,6 +63,7 @@ int gui_run() {
     );
 
     // Initialize Direct3D
+    spdlog::trace("Initializing Direct3D...");
     if (!CreateDeviceD3D(hwnd)) {
         CleanupDeviceD3D();
         ::UnregisterClassW(wc.lpszClassName, wc.hInstance);
@@ -69,60 +71,31 @@ int gui_run() {
     }
 
     // Show the window
+    spdlog::trace("Showing window...");
     ::ShowWindow(hwnd, SW_SHOWDEFAULT);
     ::UpdateWindow(hwnd);
 
     // Setup Dear ImGui context
+    spdlog::trace("Initializing ImGui...");
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO();
-    (void)io;
-    io.ConfigFlags
-        |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
-    io.ConfigFlags
-        |= ImGuiConfigFlags_NavEnableGamepad;  // Enable Gamepad Controls
+    io.IniFilename = nullptr;
+    io.LogFilename = nullptr;
 
     // Setup Dear ImGui style
     ImGui::StyleColorsDark();
-    // ImGui::StyleColorsLight();
 
     // Setup Platform/Renderer backends
     ImGui_ImplWin32_Init(hwnd);
     ImGui_ImplDX9_Init(g_pd3dDevice);
 
-    // Load Fonts
-    // - If no fonts are loaded, dear imgui will use the default font. You can
-    // also load multiple fonts and use ImGui::PushFont()/PopFont() to select
-    // them.
-    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you
-    // need to select the font among multiple.
-    // - If the file cannot be loaded, the function will return a nullptr.
-    // Please handle those errors in your application (e.g. use an assertion, or
-    // display an error and quit).
-    // - The fonts will be rasterized at a given size (w/ oversampling) and
-    // stored into a texture when calling
-    // ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame
-    // below will call.
-    // - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use
-    // Freetype for higher quality font rendering.
-    // - Read 'docs/FONTS.md' for more instructions and details.
-    // - Remember that in C/C++ if you want to include a backslash \ in a string
-    // literal you need to write a double backslash \\ !
-    // io.Fonts->AddFontDefault();
-    // io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\segoeui.ttf", 18.0f);
-    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-    // ImFont* font =
-    // io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f,
-    // nullptr, io.Fonts->GetGlyphRangesJapanese()); IM_ASSERT(font != nullptr);
+    // Load Fonts (nothing here yet)
 
-    // Our state
-    bool show_demo_window = true;
-    bool show_another_window = false;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    // Initialize state (nothing here yet)
 
     // Main loop
+    spdlog::trace("Starting main loop...");
     bool done = false;
     while (!done) {
         // Poll and handle messages (inputs, window resize, etc.)
@@ -160,80 +133,23 @@ int gui_run() {
         ImGui_ImplDX9_NewFrame();
         ImGui_ImplWin32_NewFrame();
         ImGui::NewFrame();
-
-        // 1. Show the big demo window (Most of the sample code is in
-        // ImGui::ShowDemoWindow()! You can browse its code to learn more about
-        // Dear ImGui!).
-        if (show_demo_window) ImGui::ShowDemoWindow(&show_demo_window);
-
-        // 2. Show a simple window that we create ourselves. We use a Begin/End
-        // pair to create a named window.
-        {
-            static float f = 0.0f;
-            static int counter = 0;
-
-            ImGui::Begin("Hello, world!"
-            );  // Create a window called "Hello, world!" and append into it.
-
-            ImGui::Text("This is some useful text."
-            );  // Display some text (you can use a format strings too)
-            ImGui::Checkbox(
-                "Demo Window", &show_demo_window
-            );  // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat(
-                "float", &f, 0.0f, 1.0f
-            );  // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3(
-                "clear color", (float*)&clear_color
-            );  // Edit 3 floats representing a color
-
-            if (ImGui::Button("Button"
-                ))  // Buttons return true when clicked (most widgets return
-                    // true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
-            ImGui::Text(
-                "Application average %.3f ms/frame (%.1f FPS)",
-                1000.0f / io.Framerate,
-                io.Framerate
-            );
-            ImGui::End();
-        }
-
-        // 3. Show another simple window.
-        if (show_another_window) {
-            ImGui::Begin(
-                "Another Window", &show_another_window
-            );  // Pass a pointer to our bool variable (the window will have a
-                // closing button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me")) show_another_window = false;
-            ImGui::End();
-        }
+        ImGui::SetNextWindowSize({io.DisplaySize.x, io.DisplaySize.y});
+        ImGui::SetNextWindowPos({0, 0});
+        ImGui::Begin(
+            "Hitman Tracker",
+            nullptr,
+            ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize
+                | ImGuiWindowFlags_NoMove
+        );
+        ImGui::Text("Game Not Running");
+        ImGui::End();
+        ImGui::EndFrame();
 
         // Rendering
         ImGui::EndFrame();
         g_pd3dDevice->SetRenderState(D3DRS_ZENABLE, FALSE);
         g_pd3dDevice->SetRenderState(D3DRS_ALPHABLENDENABLE, FALSE);
         g_pd3dDevice->SetRenderState(D3DRS_SCISSORTESTENABLE, FALSE);
-        D3DCOLOR clear_col_dx = D3DCOLOR_RGBA(
-            (int)(clear_color.x * clear_color.w * 255.0f),
-            (int)(clear_color.y * clear_color.w * 255.0f),
-            (int)(clear_color.z * clear_color.w * 255.0f),
-            (int)(clear_color.w * 255.0f)
-        );
-        g_pd3dDevice->Clear(
-            0,
-            nullptr,
-            D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER,
-            clear_col_dx,
-            1.0f,
-            0
-        );
         if (g_pd3dDevice->BeginScene() >= 0) {
             ImGui::Render();
             ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
@@ -245,6 +161,7 @@ int gui_run() {
     }
 
     // Cleanup
+    spdlog::trace("Cleanup...");
     ImGui_ImplDX9_Shutdown();
     ImGui_ImplWin32_Shutdown();
     ImGui::DestroyContext();
