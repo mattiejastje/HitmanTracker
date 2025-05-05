55e6980a336cac82144c5c15fadafe7b882aaac8
mattiejas <mattiejastje@gmail.com>
2025-05-05 12:52:34 +0100

    H2SA: Get and display current map.

--- hitman2_silent_assassin/gui.cpp
+++ hitman2_silent_assassin/gui.cpp
@@ -1,11 +1,44 @@
 #include "gui.hpp"
 
+#include <fmt/format.h>
 #include <imgui.h>
 
+#include <string>
+#include <vector>
+
 #include "../format_duration.hpp"
+
+const std::vector<std::string> map_names
+    = {"The Gontranno Sactuary",    // 1
+       "Anathema",                  // 2
+       "St. Petersburg Stakeout",   // 3
+       "Kirov Park Meeting",        // 4
+       "Tubeway Torpedo",           // 5
+       "Invitation to a Party",     // 6
+       "Tracking Hayamoto",         // 7
+       "Hidden Valley",             // 8
+       "At the Gates",              // 9
+       "Shogun Showdown",           // 10
+       "Basement Killing",          // 11
+       "The Graveyard Shift",       // 12
+       "The Jacuzzi Job",           // 13
+       "Murder at the Bazaar",      // 14
+       "Motorcade Interception",    // 15
+       "Tunnel Rat",                // 16
+       "Temple City Ambush",        // 17
+       "The Death of Hannelore",    // 18
+       "Terminal Hospitality",      // 19
+       "St. Petersburg Revisited",  // 20
+       "Redemption at Gontranno"};  // 21
 
 void gui_hitman2_silent_assassin(const Stats& stats) {
     ImGui::Text("Hitman 2: Silent Assassin");
     ImGui::Separator();
-    ImGui::Text(format_duration(stats.time).c_str());
+    if (stats.map > 0) {
+        ImGui::Text(
+            fmt::format("#{} {}", stats.map, map_names.at(stats.map - 1))
+                .c_str()
+        );
+        ImGui::Text(format_duration(stats.time).c_str());
+    }
 }
--- hitman2_silent_assassin/stats.cpp
+++ hitman2_silent_assassin/stats.cpp
@@ -1,8 +1,48 @@
 #include "stats.hpp"
 
+#include <unordered_map>
+
 #include "../read_process_memory.hpp"
 
+struct Map {
+    int number;
+    std::string name;
+};
+
+// unordered_map for fast lookup
+const std::unordered_map<std::string, int> scenes = {
+    {R"(SCENES\C0-1\C0-1__MAIN.gms)", 1},
+    {R"(SCENES\C1-1\C1-1__MAIN.gms)", 2},
+    {R"(SCENES\C2-1\C2-1__MAIN.gms)", 3},
+    {R"(SCENES\C2-2\C2-2__MAIN.gms)", 4},
+    {R"(SCENES\C2-3\C2-3__MAIN.gms)", 5},
+    {R"(SCENES\C2-4\C2-4__MAIN.gms)", 6},
+    {R"(SCENES\C3-1\C3-1__MAIN.gms)", 7},
+    {R"(SCENES\C3-2a\C3-2a__MAIN.gms)", 8},
+    {R"(SCENES\C3-2b\C3-2b__MAIN.gms)", 9},
+    {R"(SCENES\C3-3\C3-3__MAIN.gms)", 10},
+    {R"(SCENES\C4-1\C4-1__MAIN.gms)", 11},
+    {R"(SCENES\C4-2\C4-2__MAIN.gms)", 12},
+    {R"(SCENES\C4-3\C4-3__MAIN.gms)", 13},
+    {R"(SCENES\C5-1\C5-1__MAIN.gms)", 14},
+    {R"(SCENES\C5-2\C5-2__MAIN.gms)", 15},
+    {R"(SCENES\C5-3\C5-3__MAIN.gms)", 16},
+    {R"(SCENES\C6-1\C6-1__MAIN.gms)", 17},
+    {R"(SCENES\C6-2\C6-2__MAIN.gms)", 18},
+    {R"(SCENES\C6-3\C6-3__MAIN.gms)", 19},
+    {R"(SCENES\C7-1\C7-1__MAIN.gms)", 20},
+    {R"(SCENES\C8-1\C8-1__MAIN.gms)", 21},
+};
+
 void stats_hitman2_silent_assassin(const HandlePtr& handle, Stats& stats) {
-    auto map_key
-        = read_string(handle, (const void*)0x006A6C5C, {0x98, 0xBC7}, 8);
+    auto scene
+        = read_string(handle, (const void*)0x006A6C5C, {0x98, 0xBBB}, 64);
+    spdlog::trace("Scene {}", scene);
+    auto iter = scenes.find(scene);
+    if (iter != scenes.end()) {
+        stats.map = iter->second;
+        spdlog::trace("Map {}", stats.map);
+    } else {
+        stats.map = 0;
+    };
 }
