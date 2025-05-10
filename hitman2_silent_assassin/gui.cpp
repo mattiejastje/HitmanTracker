36d98314708db51e7b5621eaa0876282bd1e6cd4
mattiejas <mattiejastje@gmail.com>
2025-05-10 19:25:13 +0100

    Fix typo.

--- hitman2_silent_assassin/gui.cpp
+++ hitman2_silent_assassin/gui.cpp
@@ -10,27 +10,27 @@
 #include "../imgui_utils.hpp"
 
 const std::vector<std::string> map_names
-    = {"The Gontranno Sactuary",    // 1
-       "Anathema",                  // 2
-       "St. Petersburg Stakeout",   // 3
-       "Kirov Park Meeting",        // 4
-       "Tubeway Torpedo",           // 5
-       "Invitation to a Party",     // 6
-       "Tracking Hayamoto",         // 7
-       "Hidden Valley",             // 8
-       "At the Gates",              // 9
-       "Shogun Showdown",           // 10
-       "Basement Killing",          // 11
-       "The Graveyard Shift",       // 12
-       "The Jacuzzi Job",           // 13
-       "Murder at the Bazaar",      // 14
-       "Motorcade Interception",    // 15
-       "Tunnel Rat",                // 16
-       "Temple City Ambush",        // 17
-       "The Death of Hannelore",    // 18
-       "Terminal Hospitality",      // 19
-       "St. Petersburg Revisited",  // 20
-       "Redemption at Gontranno"};  // 21
+    = {"The Gontranno Sactuary",      // 1
+       "Anathema",                    // 2
+       "St. Petersburg Stakeout",     // 3
+       "Kirov Park Meeting",          // 4
+       "Tubeway Torpedo",             // 5
+       "Invitation to a Party",       // 6
+       "Tracking Hayamoto",           // 7
+       "Hidden Valley",               // 8
+       "At the Gates",                // 9
+       "Shogun Showdown",             // 10
+       "Basement Killing",            // 11
+       "The Graveyard Shift",         // 12
+       "The Jacuzzi Job",             // 13
+       "Murder at the Bazaar",        // 14
+       "The Motorcade Interception",  // 15
+       "Tunnel Rat",                  // 16
+       "Temple City Ambush",          // 17
+       "The Death of Hannelore",      // 18
+       "Terminal Hospitality",        // 19
+       "St. Petersburg Revisited",    // 20
+       "Redemption at Gontranno"};    // 21
 
 void hitman2_silent_assassin::gui(
     const settings::Gui& settings, const Fonts& fonts, const Stats& stats
