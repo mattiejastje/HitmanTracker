688a0071315e9deb95259b11b620c94a12fb81d4
mattiejas <mattiejastje@gmail.com>
2025-05-13 17:37:20 +0100

    Use assert to ensure no duplicate labels.

--- hook.cpp
+++ hook.cpp
@@ -54,8 +54,7 @@ struct GetLabelPtrsVisitor {
 
     void operator()(const Label& label) {
         auto result = label_ptrs.insert({label.index, current_ptr});
-        if (!result.second)
-            logging::error("Duplicate label {}", label.index);
+        assert(!result.second);  // ensure no duplicate labels
     }
 };
 
