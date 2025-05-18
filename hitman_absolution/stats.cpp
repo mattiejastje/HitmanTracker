9afff7225fac849076cd2f17df362b7ca8b74475
mattiejas <mattiejastje@gmail.com>
2025-05-18 17:09:31 +0100

    Record evidence for each map.

--- hitman_absolution/stats.cpp
+++ hitman_absolution/stats.cpp
@@ -9,72 +9,69 @@ static int32_t map_key(int32_t level, int32_t section) {
     return (level << 8) + section;
 }
 
-enum class MaxRating { unrated, specialist, shadow, silent_assassin };
-
 struct MapInfo {
     int map;
     int num_evidence;
-    MaxRating max_rating;
 };
 
 const std::unordered_map<int32_t, MapInfo> scenes = {
-    {map_key(-1, -1), {0}},                                // game loading
-    {map_key(25, 0), {0}},                                 // menu
-    {map_key(0, 0), {1, 0, MaxRating::unrated}},           // garden
-    {map_key(0, 1), {2, 0, MaxRating::unrated}},           // greenhouse
-    {map_key(0, 2), {3, 1, MaxRating::unrated}},           // cliffside
-    {map_key(0, 3), {4, 1, MaxRating::shadow}},            // mansion ground
-    {map_key(0, 4), {5, 0, MaxRating::unrated}},           // mansion 2nd
-    {map_key(1, 0), {6, 1, MaxRating::silent_assassin}},   // king of chinatown
-    {map_key(2, 0), {7, 1, MaxRating::shadow}},            // terminus hotel
-    {map_key(2, 1), {8, 1, MaxRating::shadow}},            // upper floors
-    {map_key(3, 0), {9, 0, MaxRating::shadow}},            // burning hotel
-    {map_key(3, 1), {10, 1, MaxRating::shadow}},           // library
-    {map_key(3, 3), {11, 0, MaxRating::shadow}},           // pigeon coop
-    {map_key(3, 4), {12, 1, MaxRating::shadow}},           // shangri'la
-    {map_key(3, 5), {13, 1, MaxRating::shadow}},           // train station
-    {map_key(4, 0), {14, 1, MaxRating::shadow}},           // courtyard
-    {map_key(4, 1), {15, 1, MaxRating::silent_assassin}},  // vixen club
-    {map_key(4, 2), {16}},
-    {map_key(4, 3), {17}},
-    {map_key(4, 4), {18}},
-    {map_key(4, 5), {19}},
-    {map_key(4, 6), {20}},
-    {map_key(6, 0), {21}},
-    {map_key(6, 1), {22}},
-    {map_key(6, 2), {23}},
-    {map_key(7, 0), {24}},
-    {map_key(8, 0), {25}},
-    {map_key(9, 0), {26}},
-    {map_key(9, 1), {27}},
-    {map_key(10, 0), {28}},
-    {map_key(11, 2), {29}},
-    {map_key(11, 3), {30}},
-    {map_key(11, 4), {31}},
-    // map 32 is also (11, 4)?
-    {map_key(12, 1), {33}},
-    {map_key(12, 2), {34}},
-    // map 35 is also (12, 2)?
-    {map_key(13, 1), {36}},
-    // map 37 is also (13, 1)?
-    {map_key(14, 1), {38}},
-    {map_key(14, 2), {39}},
-    {map_key(14, 3), {40}},
-    {map_key(17, 1), {41}},
-    {map_key(17, 2), {42}},
-    {map_key(17, 3), {43}},
-    {map_key(18, 1), {44}},
-    {map_key(18, 2), {45}},
-    {map_key(18, 3), {46}},
-    {map_key(18, 5), {47}},
-    // map 48?
-    {map_key(21, 0), {49}},
-    {map_key(22, 1), {50}},
-    {map_key(22, 2), {51}},
-    {map_key(24, 0), {52}},
-    {map_key(25, 1), {53}},
-    {map_key(25, 2), {54}},
-    // map 55 is also (25, 2)?
+    {map_key(-1, -1), {0}},     // game loading
+    {map_key(25, 0), {0}},      // menu
+    {map_key(0, 0), {1, 0}},    // garden
+    {map_key(0, 1), {2, 0}},    // greenhouse
+    {map_key(0, 2), {3, 1}},    // cliffside
+    {map_key(0, 3), {4, 1}},    // mansion ground
+    {map_key(0, 4), {5, 0}},    // mansion 2nd
+    {map_key(1, 0), {6, 1}},    // king of chinatown
+    {map_key(2, 0), {7, 1}},    // terminus hotel
+    {map_key(2, 1), {8, 1}},    // upper floors
+    {map_key(3, 0), {9, 0}},    // burning hotel
+    {map_key(3, 1), {10, 1}},   // library
+    {map_key(3, 3), {11, 0}},   // pigeon coop
+    {map_key(3, 4), {12, 1}},   // shangri'la
+    {map_key(3, 5), {13, 1}},   // train station
+    {map_key(4, 0), {14, 1}},   // courtyard
+    {map_key(4, 1), {15, 1}},   // vixen club
+    {map_key(4, 2), {16, 0}},   // dressing rooms
+    {map_key(4, 3), {17, 1}},   // derelict building
+    {map_key(4, 4), {18, 1}},   // convenience store
+    {map_key(4, 5), {19, 1}},   // loading area
+    {map_key(4, 6), {20, 1}},   // chinese new year
+    {map_key(6, 0), {21, 0}},   // victoria's ward
+    {map_key(6, 1), {22, 1}},   // orphanage halls
+    {map_key(6, 2), {23, 1}},   // central heating
+    {map_key(7, 0), {24, 1}},   // great balls of fire
+    {map_key(8, 0), {25, 1}},   // gunshop
+    {map_key(9, 0), {26, 1}},   // streets of hope
+    {map_key(9, 1), {27, 1}},   // barber shop
+    {map_key(10, 0), {28, 0}},  // the desert
+    {map_key(11, 0), {29, 1}},  // dead end
+    {map_key(11, 1), {30, 1}},  // old mill
+    {map_key(11, 2), {31, 1}},  // descent
+    {map_key(11, 3), {32, 1}},  // factory compound
+    {map_key(12, 0), {33, 1}},  // test facility
+    {map_key(12, 1), {34, 1}},  // decontamination
+    {map_key(12, 2), {35, 1}},  // r&d
+    {map_key(13, 0), {36, 1}},  // patriot's hangar
+    {map_key(13, 1), {37, 1}},  // arena
+    {map_key(14, 0), {38, 1}},  // parking
+    {map_key(14, 1), {39, 1}},  // reception
+    {map_key(14, 2), {40, 1}},  // cornfield
+    {map_key(17, 0), {41, 1}},  // courthouse
+    {map_key(17, 1), {42, 1}},  // holding cells
+    {map_key(17, 2), {43, 1}},  // prison
+    {map_key(18, 0), {44, 1}},  // county jail
+    {map_key(18, 1), {45, 1}},  // outgunned
+    {map_key(18, 2), {46, 1}},  // burn
+    {map_key(18, 3), {47, 1}},  // hope fair
+    {map_key(18, 4), {48, 0}},  // church
+    {map_key(21, 0), {49, 1}},  // tailor shop
+    {map_key(22, 1), {50, 1}},  // blackwater park
+    {map_key(22, 2), {51, 1}},  // the penthouse
+    {map_key(24, 0), {52, 1}},  // blackwater roof
+    {map_key(25, 0), {53, 1}},  // cemetary entrance
+    {map_key(25, 1), {54, 1}},  // burnwood family tomb
+    {map_key(25, 2), {55, 1}},  // crematorium
 };
 
 static Status get_silent_assassin(const Stats& stats) {
