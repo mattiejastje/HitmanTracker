fa34a6cd79d7b7855a9b2284b84838c1a0d8f80f
mattiejas <mattiejastje@gmail.com>
2025-06-20 12:35:32 +0100

    Use common gui for codename and contracts.

--- hitman_codename_47/gui.cpp
+++ hitman_codename_47/gui.cpp
@@ -2,16 +2,12 @@
 
 #include <imgui.h>
 
-#include "../format_duration.hpp"
+#include "../hitman_common/gui.hpp"
 
 void hitman_codename_47::gui(
     const settings::Gui& settings, const Fonts& fonts, const Stats& stats
 ) {
-    ImGui::PushFont(fonts.title);
-    ImGui::Text("Hitman: Codename 47");
-    ImGui::PopFont();
-    ImGui::Separator();
-    ImGui::PushFont(fonts.time);
-    ImGui::Text(format_duration(stats.time).c_str());
-    ImGui::PopFont();
+    hitman_common::gui(
+        settings, fonts, stats, "Hitman: Codename 47", "", {}, {}
+    );
 }
