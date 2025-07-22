0da0112068653f9b8a3255ec275a8ee4bcd69bc1
mattiejas <mattiejastje@gmail.com>
2025-07-22 07:46:55 +0100

    Clean up get_code for integer types.

--- hook.cpp
+++ hook.cpp
@@ -5,9 +5,12 @@
 #include "logging.hpp"
 #include "mem/read_write.hpp"
 
-// note: hardcoded for 32 bit
+static Code get_code_i8(intptr_t value) {
+    assert((INT8_MIN <= value) && (value <= INT8_MAX));
+    return {static_cast<uint8_t>(value)};
+}
 
-static Code get_code(intptr_t value) {
+static Code get_code_i32(intptr_t value) {
     assert((INT32_MIN <= value) && (value <= INT32_MAX));
     return {
         static_cast<uint8_t>(value),
@@ -103,20 +106,19 @@ struct GetCodeVisitor {
         current_ptr += jump.code.size() + 4;
         auto offset
             = std::visit(GetPtrVisitor{label_ptrs}, jump.ptr.ptr) - current_ptr;
-        return add_code(jump.code, get_code(offset));
+        return add_code(jump.code, get_code_i32(offset));
     };
 
     Code operator()(const JumpShort& jump) {
         current_ptr += jump.code.size() + 1;
         auto offset
             = std::visit(GetPtrVisitor{label_ptrs}, jump.ptr.ptr) - current_ptr;
-        assert((-128 <= offset) && (offset <= 127));
-        return add_code(jump.code, Code{static_cast<uint8_t>(offset)});
+        return add_code(jump.code, get_code_i8(offset));
     };
 
     Code operator()(const Ptr& ptr) {
         current_ptr += 4;
-        return get_code(std::visit(GetPtrVisitor{label_ptrs}, ptr.ptr));
+        return get_code_i32(std::visit(GetPtrVisitor{label_ptrs}, ptr.ptr));
     }
 
     Code operator()(const Fill& fill) {
