4ad021b293ce2d6ee4a57f067f11fb9a89154f18
mattiejas <mattiejastje@gmail.com>
2026-03-01 12:10:10 +0000

    HMA: use correct checkpoint calculation instead of relying on level manager

--- hitman_absolution/stats.cpp
+++ hitman_absolution/stats.cpp
@@ -34,21 +34,6 @@ struct GameTimeManager {
 
 static_assert(sizeof(GameTimeManager) == 0x88);
 
-// https://github.com/pavledev/HitmanAbsolutionSDK/blob/main/HitmanAbsolutionSDK/include/Glacier/ZLevelManager.h
-// https://github.com/pavledev/HitmanAbsolutionSDK/blob/main/HitmanAbsolutionSDK/include/Glacier/SSceneParameters.h
-struct LevelManager {
-    int32_t vtable;
-    int32_t scene_resource_length;
-    int32_t scene_resource_ptr;
-    int32_t game_mode;  // 1 = story mode
-    int64_t bonus_weapon;
-    int64_t bonus_outfit;
-    int32_t checkpoint_index;  // checkpoint the game was loaded from, not
-                               // necessarily current checkpoint
-    int8_t is_restoring;
-    int8_t use_savegame;
-};
-
 enum class Rating { unrated, veteran, specialist, shadow, silent_assassin };
 
 struct MapInfo {
@@ -57,135 +42,149 @@ struct MapInfo {
     Rating max_rating;
 };
 
-const std::unordered_map<std::string, std::vector<MapInfo>> scenes = {
-    {"assembly:/Scenes/L01/L01_Main.entity",
-     {
-         {1, 0},                  // 0 garden
-         {2, 0},                  // 1 greenhouse
-         {3, 1},                  // 2 cliffside
-         {4, 1, Rating::shadow},  // 3 mansion ground
-         {5, 0},                  // 4 mansion 2nd
-     }},
-    {"assembly:/Scenes/L01B/L01B_Main.entity",
-     {
-         {6, 1, Rating::silent_assassin},  // 0 king of chinatown
-     }},
-    {"assembly:/Scenes/L02B/L02B_Main.entity",
-     {
-         {7, 1, Rating::shadow},  // 0 terminus hotel
-         {8, 1, Rating::shadow},  // 1 upper floors (elevator cutscene)
-         {8, 1, Rating::shadow},  // 2 upper floors
-         {8, 1, Rating::shadow},  // 3 upper floors (after picking the lock)
-     }},
-    {"assembly:/Scenes/L02C/L02C_Main.entity",
-     {
-         {9, 0, Rating::specialist},  // 0 burning hotel
-         {10, 1, Rating::shadow},     // 1 library
-         {11, 0},                     // 2 pigeon coop (part with heli)
-         {11, 0},                     // 3 pigeon coop (part with cops)
-         {12, 1, Rating::shadow},     // 4 shangri-la
-         {13, 1, Rating::shadow},     // 5 train station (elevator cutscene)
-         {13, 1, Rating::shadow},     // 6 train station
-     }},
-    {"assembly:/Scenes/L02D/L02D_Main.entity",
-     {
-         {14, 1, Rating::shadow},           // 0 courtyard
-         {15, 1, Rating::silent_assassin},  // 1 vixen club
-         {16, 0, Rating::specialist},       // 2 dressing rooms
-         {17, 1, Rating::shadow},           // 3 derelict building
-         {18, 1, Rating::shadow},           // 4 convenience store
-         {19, 1},                           // 5 loading area
-         {20, 1, Rating::silent_assassin},  // 6 chinese new year
-         {20, 1, Rating::silent_assassin},  // 7 chinese new year (lock pick)
-     }},
-    {"assembly:/Scenes/L03/L03_Main.entity",
-     {
-         {21, 0},                           // 0 victoria's ward
-         {22, 1, Rating::shadow},           // 1 orphanage halls
-         {23, 1, Rating::silent_assassin},  // 2 central heating (cutscene)
-         {23, 1, Rating::silent_assassin},  // 3 central heating
-     }},
-    {"assembly:/Scenes/L04B/L04B_Main.entity",
-     {
-         {24, 1, Rating::shadow},  // 0 great balls of fire
-     }},
-    {"assembly:/Scenes/L04C/L04C_Main.entity",
-     {
-         {25, 1, Rating::veteran},  // 0 gunshop
-     }},
-    {"assembly:/Scenes/L04D/L04D_Main.entity",
-     {
-         {26, 1, Rating::silent_assassin},  // 0 streets of hope
-         {27, 1, Rating::silent_assassin},  // 1 barber shop
-         {27, 1, Rating::silent_assassin},  // 2 barber shop (cutscene)
-     }},
-    {"assembly:/Scenes/L04E/L04E_Main.entity",
-     {
-         {28, 0},  // 0 the desert
-     }},
-    {"assembly:/Scenes/L05A/L05A_Main.entity",
-     {
-         {29, 1, Rating::shadow},  // 0 dead end (loading)
-         {29, 1, Rating::shadow},  // 1 dead end
-         {30, 1, Rating::shadow},  // 2 old mill
-         {31, 1, Rating::shadow},  // 3 descent
-         {32, 1, Rating::shadow},  // 4 factory compound
-     }},
-    {"assembly:/Scenes/L05B/L05B_Main.entity",
-     {
-         {33, 1, Rating::silent_assassin},  // 0 test facility
-         {34, 1, Rating::shadow},           // 1 decontamination
-         {35, 1, Rating::silent_assassin},  // 2 r&d
-     }},
-    {"assembly:/Scenes/L05C/L05C_Main.entity",
-     {
-         {36, 1, Rating::shadow},           // 0 patriot's hangar
-         {37, 1, Rating::silent_assassin},  // 1 arena
-     }},
-    {"assembly:/Scenes/L06A/L06A_Main.entity",
-     {
-         {38, 1, Rating::silent_assassin},  // 0 parking
-         {39, 1, Rating::silent_assassin},  // 1 reception
-         {40, 1, Rating::silent_assassin},  // 2 cornfield
-     }},
-    {"assembly:/Scenes/L06D/L06D_Main.entity",
-     {
-         {41, 1, Rating::shadow},  // 0 courthouse
-         {42, 1},                  // 1 holding cells
-         {43, 1, Rating::shadow},  // 2 prison
-         {43, 1, Rating::shadow},  // 3 prison (cutscene)
-     }},
-    {"assembly:/Scenes/L07A/L07A_Main.entity",
-     {
-         {44, 1, Rating::shadow},  // 0 county jail
-         {45, 1, Rating::shadow},  // 1 outgunned
-         {46, 1, Rating::shadow},  // 2 burn
-         {47, 1, Rating::shadow},  // 3 hope fair
-         {48, 0},                  // 4 church (outside)
-         {48, 0},                  // 4 church (inside)
-     }},
-    {"assembly:/Scenes/L08A/L08A_Main.entity",
-     {
-         {49, 1},  // 0 tailor shop
-     }},
-    {"assembly:/Scenes/L08B/L08B_Main.entity",
-     {
-         {50, 1, Rating::shadow},           // 0 blackwater park
-         {50, 1, Rating::shadow},           // 1 blackwater park
-         {51, 1, Rating::silent_assassin},  // 2 the penthouse
-     }},
-    {"assembly:/Scenes/L09/L09_Main.entity",
-     {
-         {52, 1, Rating::silent_assassin},  // 0 blackwater roof
-     }},
-    {"assembly:/Scenes/L10/L10_Main.entity",
-     {
-         {53, 1, Rating::shadow},           // 0 cemetary entrance
-         {54, 1, Rating::silent_assassin},  // 1 burnwood family tomb
-         {55, 1, Rating::silent_assassin},  // 2 crematorium
-     }},
+// level, checkpoint -> map info
+const std::vector<std::vector<MapInfo>> scenes = {
+    // level 0
+    {
+        {1, 0},                  // 0 garden
+        {2, 0},                  // 1 greenhouse
+        {3, 1},                  // 2 cliffside
+        {4, 1, Rating::shadow},  // 3 mansion ground
+        {5, 0},                  // 4 mansion 2nd
+    },
+    // level 1
+    {
+        {6, 1, Rating::silent_assassin},  // 0 king of chinatown
+    },
+    // level 2
+    {
+        {7, 1, Rating::shadow},  // 0 terminus hotel
+        {8, 1, Rating::shadow},  // 1 upper floors (elevator cutscene)
+        {8, 1, Rating::shadow},  // 2 upper floors
+        {8, 1, Rating::shadow},  // 3 upper floors (after picking the lock)
+    },
+    // level 3
+    {
+        {9, 0, Rating::specialist},  // 0 burning hotel
+        {10, 1, Rating::shadow},     // 1 library
+        {11, 0},                     // 2 pigeon coop (part with heli)
+        {11, 0},                     // 3 pigeon coop (part with cops)
+        {12, 1, Rating::shadow},     // 4 shangri-la
+        {13, 1, Rating::shadow},     // 5 train station (elevator cutscene)
+        {13, 1, Rating::shadow},     // 6 train station
+    },
+    // level 4
+    {
+        {14, 1, Rating::shadow},           // 0 courtyard
+        {15, 1, Rating::silent_assassin},  // 1 vixen club
+        {16, 0, Rating::specialist},       // 2 dressing rooms
+        {17, 1, Rating::shadow},           // 3 derelict building
+        {18, 1, Rating::shadow},           // 4 convenience store
+        {19, 1},                           // 5 loading area
+        {20, 1, Rating::silent_assassin},  // 6 chinese new year
+        {20, 1, Rating::silent_assassin},  // 7 chinese new year (lock pick)
+    },
+    // level 5
+    {},
+    // level 6
+    {
+        {21, 0},                           // 0 victoria's ward
+        {22, 1, Rating::shadow},           // 1 orphanage halls
+        {23, 1, Rating::silent_assassin},  // 2 central heating (cutscene)
+        {23, 1, Rating::silent_assassin},  // 3 central heating
+    },
+    // level 7
+    {
+        {24, 1, Rating::shadow},  // 0 great balls of fire
+    },
+    // level 8
+    {
+        {25, 1, Rating::veteran},  // 0 gunshop
+    },
+    // level 9
+    {
+        {26, 1, Rating::silent_assassin},  // 0 streets of hope
+        {27, 1, Rating::silent_assassin},  // 1 barber shop
+        {27, 1, Rating::silent_assassin},  // 2 barber shop (cutscene)
+    },
+    // level 10
+    {
+        {28, 0},  // 0 the desert
+    },
+    // level 11
+    {
+        {29, 1, Rating::shadow},  // 0 dead end (loading)
+        {29, 1, Rating::shadow},  // 1 dead end
+        {30, 1, Rating::shadow},  // 2 old mill
+        {31, 1, Rating::shadow},  // 3 descent
+        {32, 1, Rating::shadow},  // 4 factory compound
+    },
+    // level 12
+    {
+        {33, 1, Rating::silent_assassin},  // 0 test facility
+        {34, 1, Rating::shadow},           // 1 decontamination
+        {35, 1, Rating::silent_assassin},  // 2 r&d
+    },
+    // level 13
+    {
+        {36, 1, Rating::shadow},           // 0 patriot's hangar
+        {37, 1, Rating::silent_assassin},  // 1 arena
+    },
+    // level 14
+    {
+        {38, 1, Rating::silent_assassin},  // 0 parking
+        {39, 1, Rating::silent_assassin},  // 1 reception
+        {40, 1, Rating::silent_assassin},  // 2 cornfield
+    },
+    // level 15
+    {},
+    // level 16
+    {},
+    // level 17
+    {
+        {41, 1, Rating::shadow},  // 0 courthouse
+        {42, 1},                  // 1 holding cells
+        {43, 1, Rating::shadow},  // 2 prison
+        {43, 1, Rating::shadow},  // 3 prison (cutscene)
+    },
+    // level 18
+    {
+        {44, 1, Rating::shadow},  // 0 county jail
+        {45, 1, Rating::shadow},  // 1 outgunned
+        {46, 1, Rating::shadow},  // 2 burn
+        {47, 1, Rating::shadow},  // 3 hope fair
+        {48, 0},                  // 4 church (outside)
+        {48, 0},                  // 4 church (inside)
+    },
+    // level 19
+    {},
+    // level 20
+    {},
+    // level 21
+    {
+        {49, 1},  // 0 tailor shop
+    },
+    // level 22
+    {
+        {50, 1, Rating::shadow},           // 0 blackwater park
+        {50, 1, Rating::shadow},           // 1 blackwater park
+        {51, 1, Rating::silent_assassin},  // 2 the penthouse
+    },
+    // level 23
+    {},
+    // level 24
+    {
+        {52, 1, Rating::silent_assassin},  // 0 blackwater roof
+    },
+    // level 25
+    {
+        {53, 1, Rating::shadow},           // 0 cemetary entrance
+        {54, 1, Rating::silent_assassin},  // 1 burnwood family tomb
+        {55, 1, Rating::silent_assassin},  // 2 crematorium
+    },
 };
 
+/*
 static Status get_rating_status(Rating max_rating, const Stats& stats) {
     return (max_rating != Rating::unrated && stats.spotted.value != 0)
                ? Status::RED
@@ -209,6 +208,97 @@ static std::string get_rating_value(Rating max_rating, Status status) {
                                            : "Silent Assassin"
     );
 }
+*/
+
+constexpr int32_t NUM_LEVELS = 26;
+constexpr int32_t NUM_CHECKPOINTS_PER_LEVEL
+    = 13;  // hard upper bound (seen in assembly code)
+
+struct CheckpointManager {
+    int8_t _pad0[0x24];
+    int32_t unknown_24;                // +0x24
+    int32_t checkpoint_container_ptr;  // +0x28
+};
+
+struct CheckpointContainer {
+    int8_t _pad0[0x0C];
+    int32_t keys_start_ptr;  // +0x0C points to entry[0].key
+    int32_t keys_end_ptr;    // +0x10 points one past last entry
+    int8_t _pad1[0x3C];      // ...
+    int32_t current_key;     // +0x50
+};
+
+static int32_t get_level(void* handle, const BasePtrs& base_ptrs) {
+    auto level = read<int32_t>(handle, base_ptrs[0] + 0xE21394);
+    if (!level) {
+        logging::error("Unable to read level");
+        return -1;
+    }
+    auto level_value = level.value();
+    // engine may set level to -1 if not in a mission
+    // sadly it's not a reliable way to detect if we are in a mission
+    if (level_value < -1 || level_value >= NUM_LEVELS) {
+        logging::error("Level {} out of bounds", level_value);
+        return -1;
+    }
+    return level_value;
+}
+
+// replicate game engine logic for calculating checkpoint
+static int32_t get_checkpoint(void* handle, const BasePtrs& base_ptrs) {
+    const auto manager
+        = read<CheckpointManager>(handle, base_ptrs[0] + 0xE21580);
+    if (!manager) {
+        logging::error("Unable to read checkpoint manager");
+        return -1;
+    }
+    if (manager->checkpoint_container_ptr == 0) {
+        // in main menu and haven't loaded level yet
+        logging::debug("Checkpoint container pointer is null");
+        return -1;
+    }
+    const auto container
+        = read<CheckpointContainer>(handle, manager->checkpoint_container_ptr);
+    if (!container) {
+        logging::error("Unable to read checkpoint container");
+        return -1;
+    }
+    if (container->current_key == 0) {
+        // level is loading but we have not started yet
+        logging::debug("Current checkpoint key is null");
+        return -1;
+    }
+    // each checkpoint entry is 8 bytes: check and calculate num_checkpoints
+    const auto total_bytes
+        = container->keys_end_ptr - container->keys_start_ptr;
+    if ((total_bytes & 7) != 0) {
+        logging::error(
+            "Checkpoint table size {} not divisible by 8", total_bytes
+        );
+        return -1;
+    }
+    const auto num_checkpoints = total_bytes >> 3;
+    logging::trace("Number of checkpoints is {}", num_checkpoints);
+    if ((num_checkpoints <= 0)
+        || (num_checkpoints > NUM_CHECKPOINTS_PER_LEVEL)) {
+        logging::error(
+            "Number of checkpoints {} out of range", num_checkpoints
+        );
+        return -1;
+    };
+    // look up current checkpoint key in the checkpoint keys table
+    for (int32_t checkpoint = 0; checkpoint < num_checkpoints; checkpoint++) {
+        auto key
+            = read<int32_t>(handle, container->keys_start_ptr + checkpoint * 8);
+        if (!key) {
+            logging::error("Unable to read from checkpoint table");
+            return -1;
+        }
+        if (key.value() == container->current_key) return checkpoint;
+    };
+    logging::error("Checkpoint key {:#x} not found", container->current_key);
+    return -1;
+}
 
 void hitman_absolution::update_slow(
     void* handle,
@@ -222,45 +312,31 @@ void hitman_absolution::update_slow(
     } else {
         logging::error("Unable to read difficulty");
     }
-    auto level_manager = read<LevelManager>(handle, base_ptrs[0] + 0xE21310);
-    if (!level_manager) {
-        logging::error("Unable to read level manager");
-        return;
-    }
-    if (level_manager.value().scene_resource_length > 64) {
-        logging::error("Scene resource length overflow");
-        return;
-    }
-    if (level_manager.value().scene_resource_length < 0
-        || level_manager.value().game_mode != 1) {
+    auto level = get_level(handle, base_ptrs);
+    stats.level = stats_value(level, false);
+    if (level < 0) {
         stats.map = 0;
         return;
     }
-    auto scene = read_string(
-        handle,
-        level_manager.value().scene_resource_ptr,
-        level_manager.value().scene_resource_length
-    );
-    if (!scene) {
-        logging::error("Unable to read scene");
+    auto checkpoint = get_checkpoint(handle, base_ptrs);
+    stats.checkpoint = stats_value(checkpoint, false);
+    if (checkpoint < 0) {
+        stats.map = 0;
         return;
     }
-    auto checkpoint
-        = read<int32_t>(handle, base_ptrs[0] + 0xD59670 + 0x7924).value_or(-1);
-    logging::trace("Scene {}, checkpoint {}", scene.value(), checkpoint);
-    auto iter = scenes.find(scene.value());
-    if (iter == scenes.end()) {
-        logging::error("No map registered for scene {}", scene.value());
+    logging::trace("Level {} at checkpoint {}", level, checkpoint);
+    if (level >= scenes.size()) {  // checked previously... but just in case
+        logging::error("No map registered for level {}", level);
         return;
-    } else if (checkpoint < 0 || checkpoint >= iter->second.size()) {
+    }
+    auto& map_infos = scenes.at(level);
+    if (checkpoint >= map_infos.size()) {
         logging::error(
-            "No map registered for checkpoint {} of scene {}",
-            checkpoint,
-            scene.value()
+            "No map registered for level {}, checkpoint {}", level, checkpoint
         );
         return;
     }
-    auto& map_info = iter->second.at(checkpoint);
+    auto& map_info = map_infos.at(checkpoint);
     logging::trace("Map {}", map_info.map);
     stats.map = map_info.map;
     stats.map_stage = MapStage::main;  // always render stats
--- stats.hpp
+++ stats.hpp
@@ -27,20 +27,29 @@ struct Stats {
     StatsValue<int32_t> enemies_wounded;
     StatsValue<int32_t> innocents_killed;
     StatsValue<int32_t> innocents_wounded;
-    StatsValue<int32_t> close_encounters;   // h2sa
-    StatsValue<int32_t> alerts;             // h2sa
-    StatsValue<int32_t> police_killed;      // bm
-    StatsValue<int32_t> police_wounded;     // bm
-    StatsValue<int32_t> frisk_failed;       // bm
-    StatsValue<int32_t> cover_blown;        // bm
-    StatsValue<int32_t> bodies_fnd;         // bm
-    StatsValue<int32_t> target_bodies_fnd;  // bm
-    StatsValue<int32_t> uncon_bodies_fnd;   // bm
-    StatsValue<int32_t> witnesses;          // bm
-    StatsValue<int32_t> on_camera;          // bm
-    StatsValue<int32_t> cust_weapons_left;  // bm
-    StatsValue<int32_t> suit_left;          // bm
-    StatsValue<int32_t> spotted;            // hma
-    StatsValue<int32_t> evidence_left;      // hma
-    StatsValue<int32_t> target_not_located; // hma
+    StatsValue<int32_t> close_encounters;       // h2sa, hc
+    StatsValue<int32_t> alerts;                 // h2sa, hc
+    StatsValue<int32_t> police_killed;          // bm
+    StatsValue<int32_t> police_wounded;         // bm
+    StatsValue<int32_t> frisk_failed;           // bm
+    StatsValue<int32_t> cover_blown;            // bm
+    StatsValue<int32_t> bodies_fnd;             // bm
+    StatsValue<int32_t> target_bodies_fnd;      // bm
+    StatsValue<int32_t> uncon_bodies_fnd;       // bm
+    StatsValue<int32_t> witnesses;              // bm
+    StatsValue<int32_t> on_camera;              // bm
+    StatsValue<int32_t> cust_weapons_left;      // bm
+    StatsValue<int32_t> suit_left;              // bm
+    StatsValue<int32_t> level;                  // hma (debug, to be removed)
+    StatsValue<int32_t> checkpoint;             // hma (debug, to be removed)
+    StatsValue<int32_t> silent_assassin_bonus;  // hma
+    StatsValue<int32_t> target_kill;            // hma
+    StatsValue<int32_t> spotted;                // hma
+    StatsValue<int32_t> pacification;           // hma
+    StatsValue<int32_t> objective_complete;     // hma
+    StatsValue<int32_t> evidence_removed;       // hma
+    StatsValue<int32_t> body_hidden;            // hma
+    StatsValue<int32_t> headshot;               // hma
+    StatsValue<int32_t> silent_kill;            // hma
+    StatsValue<int32_t> signature_kill;         // hma
 };
