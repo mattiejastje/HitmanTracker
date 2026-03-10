e1f999a1e799af1625a60c468d29ad668a67d8ec
mattiejas <mattiejastje@gmail.com>
2026-03-10 17:15:10 +0000

    Another field.

--- lua/hitman_absolution.lua
+++ lua/hitman_absolution.lua
@@ -119,6 +119,7 @@ struct_defs = {
             {name = "condition_min",  offset = 0x08, type_ref = T.vector(T.struct("PlaystyleCondition"))},
             {name = "condition_max",  offset = 0x14, type_ref = T.vector(T.struct("PlaystyleCondition"))},
             {name = "title",          offset = 0x20, type_ref = T.struct("String")},
+            {name = "is_unlockable",  offset = 0x34, type_ref = T.i8},  -- if listed under "unlocks"
             {name = "unknown",        offset = 0x38, type_ref = T.i32},
             {name = "percentage_min", offset = 0x3C, type_ref = T.i32},
             {name = "percentage_max", offset = 0x40, type_ref = T.i32},
