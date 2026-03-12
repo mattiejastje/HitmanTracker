7b7f94e7c22a2e968db1b9610f5fe888a08d19cf
mattiejas <mattiejastje@gmail.com>
2026-03-12 09:23:23 +0000

    lua: small refactor in printer

--- lua/printer.lua
+++ lua/printer.lua
@@ -3,28 +3,37 @@ local typedefs = require("typedefs")
 local _printers = {}
 
 -- ----------------------------------------------------------------------------
+-- Helpers
+-- ----------------------------------------------------------------------------
+
+local function print_line(addr, indent, label, value_str)
+    local pad = string.rep("  ", indent)
+    local addr_str = addr and string.format("[%08X]", addr) or "[--------]"
+    local label_str = label and (label .. ": ") or ""
+    print(addr_str .. " " .. pad .. label_str .. value_str)
+end
+
+-- ----------------------------------------------------------------------------
 -- Dispatch
 -- ----------------------------------------------------------------------------
 
 local function print_rawval(type_ref, rawval, indent, label)
     indent = indent or 0
-    local pad = string.rep("  ", indent)
-    local prefix = label and (pad .. label .. ": ") or pad
 
     if rawval == nil then
-        print(prefix .. "nil")
+        print_line(nil, indent, label, "nil")
         return
     end
 
-    local addr_str = rawval.addr and string.format("0x%X", rawval.addr) or "nil"
+    local addr = rawval.addr
 
     if rawval.error then
-        print(prefix .. "ERROR @ " .. addr_str .. ": " .. rawval.error)
+        print_line(addr, indent, label, "ERROR: " .. rawval.error)
         if rawval.value == nil then
             return
         end
     elseif rawval.value == nil then
-        print(prefix .. "nil @ " .. addr_str)
+        print_line(addr, indent, label, "nil")
         return
     end
 
@@ -32,66 +41,66 @@ local function print_rawval(type_ref, rawval, indent, label)
     if not printer then
         error("Unknown type kind: " .. type_ref.kind)
     end
-    printer(type_ref, rawval.value, rawval.addr, indent, prefix)
+    printer(type_ref, rawval.value, addr, indent, label)
 end
 
 -- ----------------------------------------------------------------------------
 -- Printers
 -- ----------------------------------------------------------------------------
 
-_printers.array = function(type_ref, value, addr, indent, prefix)
-    print(prefix .. "array(" .. #value .. ") @ " .. string.format("0x%X", addr))
+_printers.array = function(type_ref, value, addr, indent, label)
+    print_line(addr, indent, label, "array(" .. #value .. ")")
     for i, elem in ipairs(value) do
         print_rawval(type_ref.type_ref, elem, indent + 1, "[" .. i - 1 .. "]")
     end
 end
 
-_printers.circular_list = function(type_ref, value, addr, indent, prefix)
-    print(prefix .. "circular_list(" .. #value .. ") @ " .. string.format("0x%X", addr))
+_printers.circular_list = function(type_ref, value, addr, indent, label)
+    print_line(addr, indent, label, "circular_list(" .. #value .. ")")
     for i, elem in ipairs(value) do
         print_rawval(type_ref.type_ref, elem, indent + 1, "[" .. i - 1 .. "]")
     end
 end
 
-_printers.float = function(type_ref, value, addr, indent, prefix)
-    print(prefix .. tostring(value) .. " @ " .. string.format("0x%X", addr))
+_printers.float = function(type_ref, value, addr, indent, label)
+    print_line(addr, indent, label, tostring(value))
 end
 
-_printers.i8 = function(type_ref, value, addr, indent, prefix)
-    print(prefix .. tostring(value) .. " @ " .. string.format("0x%X", addr))
+_printers.i8 = function(type_ref, value, addr, indent, label)
+    print_line(addr, indent, label, tostring(value))
 end
 
-_printers.i16 = function(type_ref, value, addr, indent, prefix)
-    print(prefix .. tostring(value) .. " @ " .. string.format("0x%X", addr))
+_printers.i16 = function(type_ref, value, addr, indent, label)
+    print_line(addr, indent, label, tostring(value))
 end
 
-_printers.i32 = function(type_ref, value, addr, indent, prefix)
-    print(prefix .. tostring(value) .. " @ " .. string.format("0x%X", addr))
+_printers.i32 = function(type_ref, value, addr, indent, label)
+    print_line(addr, indent, label, tostring(value))
 end
 
-_printers.i64 = function(type_ref, value, addr, indent, prefix)
-    print(prefix .. tostring(value) .. " @ " .. string.format("0x%X", addr))
+_printers.i64 = function(type_ref, value, addr, indent, label)
+    print_line(addr, indent, label, tostring(value))
 end
 
-_printers.ptr = function(type_ref, value, addr, indent, prefix)
+_printers.ptr = function(type_ref, value, addr, indent, label)
     if type_ref.weak then
-        print(prefix .. string.format("0x%X", value) .. " @ " .. string.format("0x%X", addr))
+        print_line(addr, indent, label, string.format("0x%X", value))
     else
-        print(prefix .. "ptr @ " .. string.format("0x%X", addr))
+        print_line(addr, indent, label, "ptr")
         print_rawval(type_ref.type_ref, value, indent + 1, nil)
     end
 end
 
-_printers.string = function(type_ref, value, addr, indent, prefix)
-    print(prefix .. '"' .. value .. '" @ ' .. string.format("0x%X", addr))
+_printers.string = function(type_ref, value, addr, indent, label)
+    print_line(addr, indent, label, '"' .. value .. '"')
 end
 
-_printers.struct = function(type_ref, value, addr, indent, prefix)
+_printers.struct = function(type_ref, value, addr, indent, label)
     local def = typedefs.struct_defs[type_ref.name]
     if not def then
         error("Unknown struct: " .. type_ref.name)
     end
-    print(prefix .. type_ref.name .. " @ " .. string.format("0x%X", addr))
+    print_line(addr, indent, label, type_ref.name)
     for _, field in ipairs(def.fields) do
         if field.print ~= false then
             print_rawval(field.type_ref, value[field.name], indent + 1, field.name)
@@ -99,8 +108,8 @@ _printers.struct = function(type_ref, value, addr, indent, prefix)
     end
 end
 
-_printers.vector = function(type_ref, value, addr, indent, prefix)
-    print(prefix .. "vector(" .. #value .. ") @ " .. string.format("0x%X", addr))
+_printers.vector = function(type_ref, value, addr, indent, label)
+    print_line(addr, indent, label, "vector(" .. #value .. ")")
     for i, elem in ipairs(value) do
         print_rawval(type_ref.type_ref, elem, indent + 1, "[" .. i - 1 .. "]")
     end
