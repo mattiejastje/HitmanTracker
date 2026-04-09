ec981ee8d6fcd0630dcea1a705f9b15949544ae6
mattiejas <mattiejastje@gmail.com>
2026-04-09 10:57:05 +0200

    Remove GameTimeManager, read game_time directly.

--- hitman_absolution/stats.cpp
+++ hitman_absolution/stats.cpp
@@ -12,33 +12,6 @@
 #include "../mem/read_write.hpp"
 #include "structs.hpp"
 
-// https://github.com/pavledev/HitmanAbsolutionSDK/blob/4599042d197d8ead470c66ea002e6a9e573ae8ae/HitmanAbsolutionSDK/include/Glacier/ZGameTimeManager.h
-struct GameTimeManager {
-    int32_t vtable;
-    int32_t _pad0;
-    int64_t ticks_per_second;
-    int64_t last_time_ticks;
-    int64_t game_time;
-    int64_t game_time_prev;
-    int64_t game_time_delta;
-    int64_t real_time;
-    int64_t real_time_prev;
-    int64_t real_time_delta;
-    float game_time_multiplier;
-    float debug_time_multiplier;
-    int64_t frame_wait;
-    int64_t frame_step;
-    int64_t frame_remain;
-    int32_t is_paused;
-    int32_t frame_count;
-    int32_t is_forced_time_step;
-    int32_t _pad1;
-    double forced_time_step;
-    double forced_time_target;
-};
-
-static_assert(sizeof(GameTimeManager) == 0x88);
-
 enum class Rating { unrated, veteran, specialist, shadow, silent_assassin };
 
 struct MapInfo {
@@ -346,9 +319,9 @@ void hitman_absolution::update_fast(
     Stats& stats
 ) {
     if (stats.map > 0) {
-        auto time = read<GameTimeManager>(handle, base_ptrs[0] + 0xE24730);
-        if (time) {
-            stats.time = time.value().game_time * time_scale;
+        auto game_time = read<int64_t>(handle, base_ptrs[0] + 0xE24730 + 0x18);
+        if (game_time) {
+            stats.time = game_time.value() * time_scale;
         } else {
             logging::error("Unable to read time");
         }
