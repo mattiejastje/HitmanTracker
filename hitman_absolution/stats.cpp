3d11c482122ed6ec5e473964b4b41896d98a4ea8
mattiejas <mattiejastje@gmail.com>
2026-04-10 13:27:28 +0200

    hma: formatting

--- hitman_absolution/stats.cpp
+++ hitman_absolution/stats.cpp
@@ -254,9 +254,8 @@ bool hitman_absolution::update_slow(
     // TODO check performance against OkTracer
     auto tracer
         = mempeep::LogTracer{MempeepOnLogEntry{}, mempeep::LogLevel::VALUES};
-    if (!mempeep::read<structs::TGame>(base_ptrs[0], reader, tracer, game)) {
+    if (!mempeep::read<structs::TGame>(base_ptrs[0], reader, tracer, game))
         return false;
-    }
     stats.difficulty = game.difficulty;
     // engine may set level to -1 if not in a mission
     // sadly it's not a reliable way to detect if we are in a mission
