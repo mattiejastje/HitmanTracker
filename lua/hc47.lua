ca1f08cd369a52268fe98133fe60ecc27a9f8bf2
mattiejas <mattiejastje@gmail.com>
2026-05-01 13:29:50 +0100

    hc47: small lua updates

--- lua/hc47.lua
+++ lua/hc47.lua
@@ -17,10 +17,10 @@ local EngineData = d.Struct("EngineData", {
     d.Field(d.Float, "unk_a71"),  -- compared with HitmanDlc+1F03C4 i.e. 0
     d.Field(d.Float, "unk_a75"),  -- compared with HitmanDlc+1F03C4 i.e. 0
     d.Seek(0x37B5),
-    d.Field(d.Double, "game_time"),  -- total scaled time
-    d.Field(d.Double, "game_raw_time"),  -- total raw time
-    d.Field(d.Double, "game_time_copy"),
-    d.Field(d.Double, "game_time_previous"),
+    d.Field(d.Double, "game_time"),  -- total time with scaling/smoothing
+    d.Field(d.Double, "real_time"),  -- total time without scaling/smoothing
+    d.Field(d.Double, "game_time_copy"),  -- seems to hold a copy of game_time?
+    d.Field(d.Double, "game_time_previous"),  -- game_time from previous frame
     d.Seek(0x37E5),
     d.Field(d.List(Node, "next_node", d.list_kind.NULL_TERMINATED, 0x1000), "nodes"),
     d.Seek(0x38F0),
@@ -31,7 +31,7 @@ local EngineData = d.Struct("EngineData", {
     d.Seek(0x3BCA),
     d.Field(d.Int8, "unk_flag_3bca"),
     d.Field(d.Int64, "cpu_cycles"),
-    d.Field(d.Double, "game_raw_time_delta"),  -- smoothed delta time
+    d.Field(d.Double, "frame_time_smoothed"),  -- smoothed time spent on each frame
     d.Seek(0x3BDB),
     d.Field(d.RawAddr(), "unk_3bdb"),  -- virtual function
 })
