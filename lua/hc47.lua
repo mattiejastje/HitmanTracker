39d555a868698d8565e0b9428e6fffa0f26fea79
mattiejas <mattiejastje@gmail.com>
2026-05-01 17:19:33 +0100

    hc47: improve padding in lua code

--- lua/hc47.lua
+++ lua/hc47.lua
@@ -21,7 +21,7 @@ local EngineData = d.Struct("EngineData", {
     d.Field(d.Double, "real_time"),  -- total time without scaling/smoothing
     d.Field(d.Double, "game_time_copy"),  -- seems to hold a copy of game_time?
     d.Field(d.Double, "game_time_previous"),  -- game_time from previous frame
-    d.Seek(0x37E5),
+    d.Skip(0x10),
     d.Field(d.List(Node, "next_node", d.list_kind.NULL_TERMINATED, 0x1000), "nodes"),
     d.Seek(0x38F0),
     d.Field(d.Int8, "unk_flag_38f0"),
@@ -32,7 +32,6 @@ local EngineData = d.Struct("EngineData", {
     d.Field(d.Int8, "unk_flag_3bca"),
     d.Field(d.Int64, "cpu_cycles"),
     d.Field(d.Double, "frame_time_smoothed"),  -- smoothed time spent on each frame
-    d.Seek(0x3BDB),
     d.Field(d.RawAddr(), "unk_3bdb"),  -- virtual function
 })
 
