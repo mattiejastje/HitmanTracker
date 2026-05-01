08a8a38f175d5d0e5dfb85ac04973b8f3d0cbeb9
mattiejas <mattiejastje@gmail.com>
2026-05-01 13:02:47 +0100

    hc47: update EngineData structure with new findings about game time

--- lua/hc47.lua
+++ lua/hc47.lua
@@ -10,9 +10,30 @@ local Node = d.Struct("Node", {
 
 local EngineData = d.Struct("EngineData", {
     d.Seek(0x59),
-    d.Field(d.RawAddr(), "unknown"),  -- event manager?
+    d.Field(d.RawAddr(), "unk_59"),  -- event manager?
+    d.Seek(0xA61),
+    d.Field(d.Double, "cpu_cycles_per_second"),
+    d.Skip(0x8),
+    d.Field(d.Float, "unk_a71"),  -- compared with HitmanDlc+1F03C4 i.e. 0
+    d.Field(d.Float, "unk_a75"),  -- compared with HitmanDlc+1F03C4 i.e. 0
+    d.Seek(0x37B5),
+    d.Field(d.Double, "game_time"),  -- total scaled time
+    d.Field(d.Double, "game_raw_time"),  -- total raw time
+    d.Field(d.Double, "game_time_copy"),
+    d.Field(d.Double, "game_time_previous"),
     d.Seek(0x37E5),
     d.Field(d.List(Node, "next_node", d.list_kind.NULL_TERMINATED, 0x1000), "nodes"),
+    d.Seek(0x38F0),
+    d.Field(d.Int8, "unk_flag_38f0"),
+    d.Field(d.Int8, "timestep_mode"),  -- 1 fixed time (each frame 0.03 seconds), 0 real time
+    d.Seek(0x398E),
+    d.Field(d.Float, "game_speed_scale"),  -- time scale factor (normally 1.0)
+    d.Seek(0x3BCA),
+    d.Field(d.Int8, "unk_flag_3bca"),
+    d.Field(d.Int64, "cpu_cycles"),
+    d.Field(d.Double, "game_raw_time_delta"),  -- smoothed delta time
+    d.Seek(0x3BDB),
+    d.Field(d.RawAddr(), "unk_3bdb"),  -- virtual function
 })
 
 local Engine = d.Struct("Engine", {
@@ -23,6 +44,8 @@ M.HitmanDlc = d.Struct("HitmanDlc", {
     d.Seek(0x1F0008),
     d.Field(d.RawAddr(), "unknown"),  -- memory manager?
     d.Field(d.Ref(Engine), "engine"),
+    d.Seek(0x1F03C4),
+    d.Field(d.Float, "unk_zero"),  -- seems always zero
 })
 
 return M
