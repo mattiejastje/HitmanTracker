c9b32ebe4851fb3b1f886eaa0a8dcfbb92ace92b
mattiejas <mattiejastje@gmail.com>
2026-05-14 17:03:50 +0100

    Fix

--- lua/main.lua
+++ lua/main.lua
@@ -36,6 +36,6 @@ write_structs_cpp("hitman_absolution", { hma.Game }, "hitman_absolution::structs
 write_structs_txt("hitman_blood_money", { hbm.Game })
 write_structs_cpp("hitman_blood_money", { hbm.Game }, "hitman_blood_money::structs")
 write_structs_txt("hitman_codename_47", { hc47.HitmanDlc })
-write_structs_txt("hitman2_silent_assassin", { h2sa.Game, h2sa.PlayerEntity })
+write_structs_txt("hitman2_silent_assassin", { h2sa.Game, h2sa.LevelControl })
 write_structs_txt("hitman_contracts", { hcon.HitmanContracts })
 write_structs_cpp("hitman_contracts", { hcon.HitmanContractsCpp }, "hitman_contracts::structs")
