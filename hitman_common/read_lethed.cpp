bc40d3e2a307ad73a6afaf8f461acc7828bf90f6
mattiejas <mattiejastje@gmail.com>
2026-07-10 09:47:59 +0100

    hitman_common: refactor property read and fix small bug

--- hitman_common/read_lethed.cpp
+++ hitman_common/read_lethed.cpp
@@ -4,6 +4,66 @@
 
 #include "../hitman2_silent_assassin/structs.hpp"
 
+struct PropertyRecordResult {
+    std::optional<int32_t> value;
+    int32_t record_size;
+};
+
+static std::optional<PropertyRecordResult> read_property_record(
+    uint32_t address,
+    std::string_view key,
+    MemoryReader<uint32_t>& reader,
+    LogTracer<MempeepOnLogEntry>& tracer
+) {
+    mempeep::RemoteValue<
+        hitman2_silent_assassin::structs::TPropertyManagerRecord,
+        uint32_t>
+        remote_record{address};
+    hitman2_silent_assassin::structs::PropertyManagerRecord record{};
+    if (!mempeep::read(remote_record, reader, tracer, record)) {
+        spdlog::warn("Unable to read property manager record");
+        return {};
+    }
+    // record_size - 4 bytes
+    // is_active - 1 byte
+    // property.key_length - 4 bytes
+    // property.type - 4 bytes
+    // property.size - 4 bytes
+    // property.key - ... (at least 1 byte)
+    // so record_size > 4+1+4+4+4 = 0x11
+    if (record.record_size <= 0x11) {
+        spdlog::warn("Invalid property record size");
+        return {};
+    }
+    // uint32_t property has record size
+    // 0x11 (header) + key.size() + 1 (null) + 4 (value)
+    // so filter on that first
+    if (record.is_active && record.record_size == 0x11 + key.size() + 1 + 4) {
+        hitman2_silent_assassin::structs::Property property{};
+        if (!mempeep::read(record.property, reader, tracer, property)) {
+            spdlog::warn("Unable to read property");
+            return {};
+        }
+        if (property.key == key) {
+            if (property.size != 4) {
+                spdlog::warn("Property \"{}\" has wrong size", key);
+                return {};
+            }
+            int32_t val;
+            if (!reader(address + 0x11 + key.size() + 1, 4, &val)) {
+                spdlog::warn("Unable to read property \"{}\" value", key);
+                return {};
+            }
+            return PropertyRecordResult{
+                .value = val, .record_size = record.record_size
+            };
+        }
+    }
+    return PropertyRecordResult{
+        .value = std::nullopt, .record_size = record.record_size
+    };
+}
+
 std::optional<int32_t> hitman_common::read_property_int32(
     uint32_t data,
     int32_t data_used,
@@ -18,50 +78,10 @@ std::optional<int32_t> hitman_common::read_property_int32(
             spdlog::warn("Too many properties");
             break;
         }
-        mempeep::RemoteValue<
-            hitman2_silent_assassin::structs::TPropertyManagerRecord,
-            uint32_t>
-            remote_record{data + offset};
-        hitman2_silent_assassin::structs::PropertyManagerRecord record{};
-        if (!mempeep::read(remote_record, reader, tracer, record)) {
-            spdlog::warn("Unable to read property manager record");
-            return {};
-        }
-        // record_size - 4 bytes
-        // is_active - 1 byte
-        // property.key_length - 4 bytes
-        // property.type - 4 bytes
-        // property.size - 4 bytes
-        // property.key - ... (at least 1 byte)
-        // so record_size > 4+1+4+4+4 = 0x11
-        if (record.record_size <= 0x11) {
-            spdlog::warn("Invalid property record size");
-            return {};
-        }
-        // uint32_t property has record size
-        // 0x11 (header) + key.size() + 1 (null) + 4 (value)
-        // so filter on that first
-        if (record.is_active
-            && record.record_size == 0x11 + key.size() + 1 + 4) {
-            hitman2_silent_assassin::structs::Property property{};
-            if (!mempeep::read(record.property, reader, tracer, property)) {
-                spdlog::warn("Unable to read property");
-                return {};
-            }
-            if (property.key == key) {
-                if (property.size != 4) {
-                    spdlog::warn("Property \"{}\" has wrong size", key);
-                    return {};
-                }
-                int32_t val;
-                if (!reader(data + offset + 0x18, 4, &val)) {
-                    spdlog::warn("Unable to read property \"{}\" value", key);
-                    return {};
-                }
-                return val;
-            }
-        }
-        offset += record.record_size;
+        auto result = read_property_record(data + offset, key, reader, tracer);
+        if (!result) return {};
+        if (result->value) return result->value;
+        offset += result->record_size;
     }
     return {};
 }
