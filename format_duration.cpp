c5194d4210d3d52c284b5da80c753dc44441589e
mattiejas <mattiejastje@gmail.com>
2026-07-13 13:18:42 +0100

    Simplify format_duration implementation.

--- format_duration.cpp
+++ format_duration.cpp
@@ -5,14 +5,12 @@
 #include <chrono>
 
 std::string format_duration(float seconds) {
-    std::chrono::duration<float> duration(seconds);
-    auto mins = std::chrono::duration_cast<std::chrono::minutes>(duration);
-    duration -= mins;
-    auto secs = std::chrono::duration_cast<std::chrono::seconds>(duration);
-    duration -= secs;
-    auto millisecs
-        = std::chrono::duration_cast<std::chrono::milliseconds>(duration);
+    using namespace std::chrono;
+    hh_mm_ss hms{round<milliseconds>(duration<float>(seconds))};
     return fmt::format(
-        "{}:{:02}.{:03}", mins.count(), secs.count(), millisecs.count()
+        "{}:{:02}.{:03}",
+        hms.minutes().count(),
+        hms.seconds().count(),
+        hms.subseconds().count()
     );
 }
