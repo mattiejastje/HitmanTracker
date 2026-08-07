4a2147fe0fbcabd667ff53019e5931b7d26b44bf
mattiejas <mattiejastje@gmail.com>
2026-08-07 10:39:50 +0100

    hm1: simplify game_info

--- hitman_2016/game_info.cpp
+++ hitman_2016/game_info.cpp
@@ -5,40 +5,34 @@
 void hitman_2016::register_game_info(
     std::vector<GameInfo>& registry, const settings::Gui& settings
 ) {
-    registry.emplace_back(
+    struct VersionSpec {
+        const char* tag_suffix;
+        const char* version_display;
+        PeId pe_id;
+    };
+
+    constexpr VersionSpec specs[] = {
         // Direct3D 11: retail/HITMAN.exe
-        GameInfo{
-            .tag = "h1-s-dx11",
-            .methods = GameMethods{
-                gui(settings, "Steam DX11"),
-                hook_nothing,
-                hook_immediately_ready,
-                stats_nothing_slow,
-                stats_nothing_fast,
-            },
-            .make_remote_state = [] { return std::any{}; },
-            .make_stats = [] { return std::any{}; },
-            .module_infos = {
-                {"hitman.exe", PeId{0x5F8ED8B9}},
-            },
-        }
-    );
-    registry.emplace_back(
-        // Direct3D 12: dx12Retail\HITMAN.exe
-        GameInfo{
-            .tag = "h1-s-dx12",
-            .methods = GameMethods{
-                gui(settings, "Steam DX12"),
-                hook_nothing,
-                hook_immediately_ready,
-                stats_nothing_slow,
-                stats_nothing_fast,
-            },
-            .make_remote_state = [] { return std::any{}; },
-            .make_stats = [] { return std::any{}; },
-            .module_infos = {
-                {"hitman.exe", PeId{0x5F8ED8D0}},
-            },
-        }
-    );
+        {"-s-dx11", "Steam DX11", PeId{0x5F8ED8B9}},
+        // Direct3D 12: dx12Retail/HITMAN.exe
+        {"-s-dx12", "Steam DX12", PeId{0x5F8ED8D0}},
+    };
+    for (const auto& spec : specs) {
+        GameMethods methods{
+            gui(settings, spec.version_display),
+            hook_nothing,
+            hook_immediately_ready,
+            stats_nothing_slow,
+            stats_nothing_fast,
+        };
+        registry.emplace_back(
+            GameInfo{
+                .tag = std::string("h1") + spec.tag_suffix,
+                .methods = std::move(methods),
+                .make_remote_state = [] { return std::any{}; },
+                .make_stats = [] { return std::any{}; },
+                .module_infos = {{"hitman.exe", spec.pe_id}},
+            }
+        );
+    }
 }
