738505cb849fecfcc8c7662d522042a50f40b66a
mattiejas <mattiejastje@gmail.com>
2026-08-07 10:33:44 +0100

    hcon: simplify game_info

--- hitman_contracts/game_info.cpp
+++ hitman_contracts/game_info.cpp
@@ -8,26 +8,35 @@
 void hitman_contracts::register_game_info(
     std::vector<GameInfo>& registry, const settings::Gui& settings
 ) {
-    registry.emplace_back(
-        GameInfo{
-            .tag = "hcon-s",
-            .methods
-            = GameMethods{gui(settings, "Steam Build 175"), hook_nothing, hook_immediately_ready, update_slow(Version::Steam), update_fast(Version::Steam)},
-            .make_remote_state
-            = [] { return std::make_any<structs::HitmanContracts>(); },
-            .make_stats = [] { return std::make_any<hitman_common::Stats>(); },
-            .module_infos = {{"hitmancontracts.exe", PeId{0x52B2D458}}},
-        }
-    );
-    registry.emplace_back(
-        GameInfo{
-            .tag = "hcon-g",
-            .methods
-            = GameMethods{gui(settings, "GOG Build 174"), hook_nothing, hook_immediately_ready, update_slow(Version::GOG), update_fast(Version::GOG)},
-            .make_remote_state
-            = [] { return std::make_any<structs::HitmanContracts>(); },
-            .make_stats = [] { return std::make_any<hitman_common::Stats>(); },
-            .module_infos = {{"hitmancontracts.exe", PeId{0x4091141B}}},
-        }
-    );
+    struct VersionSpec {
+        Version version;
+        const char* tag_suffix;
+        const char* version_display;
+        PeId pe_id;
+    };
+
+    constexpr VersionSpec specs[] = {
+        {Version::Steam, "-s", "Steam Build 175", PeId{0x52B2D458}},
+        {Version::GOG, "-g", "GOG Build 174", PeId{0x4091141B}},
+    };
+    for (const auto& spec : specs) {
+        GameMethods methods{
+            gui(settings, spec.version_display),
+            hook_nothing,
+            hook_immediately_ready,
+            update_slow(spec.version),
+            update_fast(spec.version)
+        };
+        registry.emplace_back(
+            GameInfo{
+                .tag = std::string("hcon") + spec.tag_suffix,
+                .methods = std::move(methods),
+                .make_remote_state
+                = [] { return std::make_any<structs::HitmanContracts>(); },
+                .make_stats
+                = [] { return std::make_any<hitman_common::Stats>(); },
+                .module_infos = {{"hitmancontracts.exe", spec.pe_id}},
+            }
+        );
+    }
 }
