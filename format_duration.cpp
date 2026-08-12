0abbda16e8c3a5c090606327a313dcc9b1b19d10
mattiejas <mattiejastje@gmail.com>
2026-08-12 09:28:50 +0100

    fix for negative times
    shouldn't happen but we should also not ignore it if it happens

--- format_duration.cpp
+++ format_duration.cpp
@@ -8,7 +8,8 @@ std::string format_duration(float seconds) {
     using namespace std::chrono;
     hh_mm_ss hms{round<milliseconds>(duration<float>(seconds))};
     return fmt::format(
-        "{}:{:02}.{:03}",
+        "{}{}:{:02}.{:03}",
+        hms.is_negative() ? "-" : "",
         hms.hours().count() * 60 + hms.minutes().count(),
         hms.seconds().count(),
         hms.subseconds().count()
